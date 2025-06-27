aefecc946feed4b6c9e1efe76559d71efe22b11d
108charlotte <108sarahsmith@gmail.com>
2025-06-27 11:31:55 -0400

    Added lava death

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -14,6 +14,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		sprite.stop()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,11 +23,14 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if velocity.length() > 0: 
+		sprite.play("default")
+	else: 
+		sprite.stop()
 
 	move_and_slide()
-	
-	if velocity.length() > 0: 
-		if not sprite.is_playing(): 
-			sprite.play("default")
-		else: 
-			sprite.stop()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	position = Vector2.ZERO
