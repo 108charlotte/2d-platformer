f2d22f181fae4bed9b61819fd42e037ca9b47b6f
108charlotte <108sarahsmith@gmail.com>
2025-08-31 08:24:33 -0400

    basically built entire game, had some trouble setting up github and thought I had committed at several points but ig not

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,10 +1,26 @@
 extends CharacterBody2D
 
+const SPEED = 600.0
+var JUMP_VELOCITY = -800.0
+var curr_level = 1
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+@onready var sprite = $Sprite2D
+@onready var visibility_notifier = $Sprite2D/VisibleOnScreenNotifier2D
+@onready var base_node = get_parent()
+@onready var area_2d = get_node("../Area2D")
 
-@onready var sprite = $AnimatedSprite2D
+var level_1 = load("res://level_1.tscn").instantiate()
+var level_2 = load("res://level_2.tscn").instantiate()
+
+func _ready(): 
+	area_2d.position = Vector2(2852.0, 247.0)
+	position = Vector2.ZERO
+	visibility_notifier.screen_exited.connect(_on_VisibilityNotifier2D_screen_exited)
+	base_node.call_deferred("add_child", level_1)
+
+# reset position when fall out of world
+func _on_VisibilityNotifier2D_screen_exited(): 
+	position = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,7 +30,6 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		sprite.stop()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -23,14 +38,22 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	if velocity.length() > 0: 
-		sprite.play("default")
-	else: 
-		sprite.stop()
 
 	move_and_slide()
 
-
+# victory flag
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	position = Vector2.ZERO
+	if body is CharacterBody2D:
+		call_deferred("_go_to_next")
+func _go_to_next() -> void:
+	curr_level += 1
+	if curr_level == 2: 
+		base_node.call_deferred("add_child", level_2)
+		base_node.remove_child(level_1)
+		JUMP_VELOCITY = -1200
+		position = Vector2.ZERO
+		area_2d.position = Vector2(-1135.0, -1370.0)
+	elif curr_level == 3: 
+		get_tree().change_scene_to_file("res://level_3.tscn")
+	else: 
+		get_tree().change_scene_to_file("res://victory.tscn")
